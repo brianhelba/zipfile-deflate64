7e05d126e064c2f9cacfdad741564673c51cf29c
Brian Helba <brian.helba@kitware.com>
2021-02-24 02:27:30 -0500

    Remove Python 3.6 support

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -35,7 +34,7 @@ setup(
         'Topic :: System :: Archiving',
         'Topic :: System :: Archiving :: Compression',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     packages=find_packages(),
     ext_modules=[
         Extension(
